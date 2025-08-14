deb275ac38bd04c38899ebe89d7a364a6b2b6638
Fazliddin94 <rahmatovb735@gmail.com>
2025-08-14 11:58:53 +0500

    Create dars-1.py

---
+++ lesson-1/homework/dars-1.py
@@ -0,0 +1,22 @@
+## --- 1 misol
+a = int(input("Son kiriting: "))
+p = 4*a
+s = a*a
+print(p, s)
+## --- 2 misol
+import math
+d = int(input("Diametr kiriting"))
+L = d*math.pi
+print(L)
+## --- 3 misol
+a = int(input("Son kiriting"))
+b = int(input("Son kiriting"))
+c = (a+b)/2
+print(c)
+## --- 4 misol
+a = int(input("Son kiriting"))
+b = int(input("Son kiriting"))
+c = a+b
+d = a*b
+k = a**2
+print(c, d, k)
